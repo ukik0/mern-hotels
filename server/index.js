c2f7971095efc724c9294113db71ae2d3037e092
Dima <ctaricc@mail.ru>
2022-10-26 00:39:38 +0300

    started server

--- server/index.js
+++ server/index.js
@@ -1,1 +1,13 @@
-console.log('log')
+import expres from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const app = expres()
+
+app.listen(8001,(req, res) => {
+    console.log('worked')
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
